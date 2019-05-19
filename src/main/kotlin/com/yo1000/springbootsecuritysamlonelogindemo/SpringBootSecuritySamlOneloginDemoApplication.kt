e1f566af82d05efe7a6b6ebf7a0923eb2d5667ba
yo1000 <yoichi.kikuchi.aug@gmail.com>
2019-05-19 17:12:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/yo1000/springbootsecuritysamlonelogindemo/SpringBootSecuritySamlOneloginDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.yo1000.springbootsecuritysamlonelogindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSecuritySamlOneloginDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootSecuritySamlOneloginDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/yo1000/springbootsecuritysamlonelogindemo/SpringBootSecuritySamlOneloginDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.yo1000.springbootsecuritysamlonelogindemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootSecuritySamlOneloginDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
